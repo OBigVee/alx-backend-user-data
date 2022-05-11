626fc506602c3ffc3e70de81b3b9d024cf610b28
OBigVee <adedejivictor98@gmail.com>
2022-05-11 21:50:33 +0100

    feat:
    Obfuscated sensitive data (password, date of birth)

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,9 +2,26 @@
 """obfuscated log message"""
 import logging
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, seperator):
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        NotImplementedError
+
+
+
+def filter_datum(fields:list, redaction:str, message:str, seperator:str) -> List[str]:
     """returns the log message obfuscated"""
     for i in fields:
         # re.sub(pattern, replace, string(message) );
