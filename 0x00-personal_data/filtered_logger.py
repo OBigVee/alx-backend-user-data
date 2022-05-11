c83bbad7fd143b89216480b97b40868b5823624f
OBigVee <adedejivictor98@gmail.com>
2022-05-11 22:17:27 +0100

    feat:
    Obfuscated sensitive data (password, date of birth)
    -Implemented 'format' method to filter values in incoming log records using 'filter_datum'

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,6 +4,14 @@ import logging
 import re
 from typing import List
 
+def filter_datum(fields:list, redaction:str, message:str, seperator:str) -> List[str]:
+    """returns the log message obfuscated"""
+    for i in fields:
+        # re.sub(pattern, replace, string(message) );
+        message = re.sub(
+            i + "=.*?" + seperator, i + "=" + redaction + seperator, message
+        )
+    return message
 
 class RedactingFormatter(logging.Formatter):
     """ Redacting Formatter class
@@ -13,19 +21,14 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self):
+    def __init__(self,fields:list):
         super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
 
     def format(self, record: logging.LogRecord) -> str:
-        NotImplementedError
+        return filter_datum(self.fields, self.REDACTION, super(RedactingFormatter,self).format(record), self.SEPARATOR )
 
 
 
-def filter_datum(fields:list, redaction:str, message:str, seperator:str) -> List[str]:
-    """returns the log message obfuscated"""
-    for i in fields:
-        # re.sub(pattern, replace, string(message) );
-        message = re.sub(
-            i + "=.*?" + seperator, i + "=" + redaction + seperator, message
-        )
-    return message
+
