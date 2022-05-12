7d95662eb186702bbbe259c1054ee6a0ed080756
OBigVee <adedejivictor98@gmail.com>
2022-05-12 20:12:17 +0100

    feat:
    - Add 'encrypt_password.py' file
    file encrypt users password and return a salted hased password
    - implement is_valid function
    - is_valid check valid password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python3
 """User passwords should NEVER be stored in plain text in a database."""
 import bcrypt
+from typing import Callable
 
-def hash_password(password: str)-> bytes:
+
+def hash_password(password: str) -> bytes:
+    """Encrypting passwords"""
     hashed = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
     return hashed
 
 
+def is_valid(hashed_password: Callable, password: str) -> bool:
+    """Check valid password"""
+    return True if bcrypt.checkpw(password.encode(), hashed_password) else False
