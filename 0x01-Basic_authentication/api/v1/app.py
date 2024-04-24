2d8eefdc7c95741ec4198c837b80016f300d35d8
obigvee <adedejivictor98@gmail.com>
2024-04-24 22:22:03 +0100

    Error handler: Unauthorized

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -17,7 +17,7 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 @app.errorhandler(404)
 def not_found(error) -> str:
     """Not found handler"""
-    return jsonify({"error": "Not found"}), 404
+    return jsonify({"error": "Unauthorized"}), 404
 
 
 if __name__ == "__main__":
