f9599cbf0b9788064377a5e73e5bf210f02c5203
obigvee <adedejivictor98@gmail.com>
2024-04-25 00:06:27 +0100

    task 6 :heavy_check_mark: - add (api/v1/app.py, api/v1/auth/basic_auth.py) files for basic auth

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""BasicAuth inherits from Auth"""
+  
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """_summary_
+
+    Args:
+        Auth (_type_): _description_
+    """
